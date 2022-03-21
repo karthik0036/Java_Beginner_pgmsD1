eafb7f5d34abeedb4c95ec3879d8942ecf414215
Karthik <jeefkarthik036@gmail.com>
2022-03-21 20:44:04 +0530

    sixth-added UC-1.5 pgm

---
+++ Cmd_line_dispMsg.java
@@ -0,0 +1,12 @@
+package com.bridge.D1_assignments;
+
+public class Cmd_line_dispMsg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<args.length;i++)  
+			System.out.println(args[i]);
+
+	}
+
+}
